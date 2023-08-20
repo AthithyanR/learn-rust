e97c1a174593ac130f398a097cb52a6855ee1c55
Athithyan Rengasamy <athithyan@Athithyans-MacBook-Pro.local>
2023-08-20 16:01:09 +0530

    kafka base setup

--- util.sh
+++ util.sh
@@ -1,3 +1,45 @@
 #/bin/bash
+set -e
 
-echo "hello"
+display_help() {
+   # Display Help
+   echo "Syntax: ./util.sh [command]"
+   echo
+   echo "---commands---"
+   echo "create-topic           Create a new kafka topic"
+   echo "delete-topic           Delete a kafka topic"
+   echo "purge-topic            Purge a kafka topic"
+   echo
+}
+
+create_topic() {
+    docker exec -it kafka1 /bin/kafka-topics --bootstrap-server kafka1:29092 --create --topic $1
+}
+
+delete_topic() {
+    docker exec -it kafka1 /bin/kafka-topics --bootstrap-server kafka1:29092 --delete --topic $1
+}
+
+case $1 in
+
+create-topic)
+    create_topic $2
+;;
+
+delete-topic)
+    delete_topic $2
+;;
+
+purge-topic)
+    delete_topic $2 && create_topic $2
+;;
+
+help)
+   display_help
+;;
+
+*)
+   echo "No command specified, displaying help"
+   display_help
+;;
+esac
